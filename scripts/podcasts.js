a81141ec338fc7ace96c106efbf1cd10d742c5b0
TylerASmith <gtylerasmith@gmail.com>
2016-03-02 14:28:02 -0500

    Refactored how podcasts are stored
    Now episode and title exist within Podcast

--- scripts/podcasts.js
+++ scripts/podcasts.js
@@ -4,7 +4,7 @@ var PodcastBox = React.createClass({
   render: function() {
     return (
       <div className="podcastBox">
-          <PodcastList />
+          <PodcastList data={this.props.data} />
       </div>
     );
   }
@@ -12,48 +12,43 @@ var PodcastBox = React.createClass({
 
 var PodcastList = React.createClass({
   render: function() {
+    //var podcastEntries = this.props.data.map(function(comment) {
     return (
         <div className="podcastList">
-          <Podcast /></div>
+          <Podcast 
+            imgUrl="http://static.giantbomb.com/uploads/original/0/31/2750982-beastcast_itunes.png"
+            podcastTitle="The BeastCast"
+            episodeTitle="number 40"
+            episodeDate="Monday, February 26th 2016 @ 6:00 AM">
+              A description
+            </Podcast>
+        </div>
       );
   }
 });
+//<PodcastTitle imgUrl="http://static.giantbomb.com/uploads/original/0/31/2750982-beastcast_itunes.png">The BeastCast!</PodcastTitle>
+//<Episode episodeTitle="number 40" episodeDate="Monday, February 26th 2016 @ 6:00 AM">A description</Episode>
+          
 
 var Podcast = React.createClass({
   render: function() {
     return (
         <div className="podcast">
-            <PodcastTitle imgUrl="http://static.giantbomb.com/uploads/original/0/31/2750982-beastcast_itunes.png">The BeastCast!</PodcastTitle>
-            <Episode episodeTitle="number 40" episodeDate="Monday, February 26th 2016 @ 6:00 AM">A description</Episode>
-          
+          <div className="podcastTitle">
+            <h2 style={noMargin}>{this.props.podcastTitle}</h2>
+            <a href={this.props.stieUrl}><img height="80%" src={this.props.imgUrl} alt={this.props.podcastTitle + " logo"}/></a>
+          </div>
+          <div className="episode">
+            {this.props.episodeTitle}<br/>
+            {this.props.episodeDate}<br/>
+            {this.props.children}
+            <a href={this.props.episodeDownload} className="download">Download</a>
+          </div>
         </div>
       );
   }
 });
 
-var PodcastTitle = React.createClass({
-  render: function() {
-    return (
-        <div className="podcastTitle">
-          <h2 style={noMargin}>{this.props.children}</h2>
-          <a href={this.props.stieUrl}><img height="80%" src={this.props.imgUrl} alt={this.props.children + " logo"}/></a>
-        </div>
-      );
-  }
-});        
-
-var Episode = React.createClass({
-  render: function() {
-    return (
-        <div className="episode">
-          {this.props.episodeTitle}<br/>
-          {this.props.episodeDate}<br/>
-          {this.props.children}
-          <a href={this.props.episodeDownload} className="download">Download</a>
-        </div>
-      );
-  }
-});
 
 //Inline Styles:
 var noMargin = {
@@ -63,5 +58,6 @@ var noMargin = {
 
 ReactDOM.render(
   React.createElement(PodcastBox, null),
+  //<PodcastBox data={data} />
   document.getElementById('content')
 );
